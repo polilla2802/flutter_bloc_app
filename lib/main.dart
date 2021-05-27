1fa837f52c3512711d61e45c88df6ce7f7728ae3
Jose Manuel Alarcon <polilla2802@gmail.com>
2021-05-27 09:49:00 -0500

    flutter bloc

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Title',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Bloc"),
+        ),
+        body: Center(
+          child: Text("test"),
+        ),
+      ),
+    );
+  }
+}
