298c6a78d9d382cc36b2475bebcc941759d98511
Jose Manuel Alarcon <polilla2802@gmail.com>
2021-05-29 17:16:10 -0500

    Layout Flex Test

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,78 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Title',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Bloc"),
-        ),
-        body: Center(
-          child: Text("test"),
+      title: 'Bloc',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bloc"),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.red[50],
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    height: 100,
+                    width: screenWidth,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+                color: Colors.blue[50],
+                alignment: Alignment.center,
+                height: 100,
+                child: Text("test")),
+            Container(
+                color: Colors.yellow[50],
+                alignment: Alignment.center,
+                height: 100,
+                child: Text("test")),
+            Expanded(
+              child: Container(
+                color: Colors.amber[50],
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                        color: Colors.red[50],
+                        alignment: Alignment.center,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              width: screenWidth,
+                              child: Text(
+                                "test",
+                                textAlign: TextAlign.center,
+                              ),
+                            )
+                          ],
+                        )),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
